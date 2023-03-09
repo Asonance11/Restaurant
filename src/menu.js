ea1528f116f4a4757dbb7f7f8470460f68bc6460
Oluwadabira <adigunsamuelaso@gmail.com>
2023-03-09 09:44:45 +0100

    styling of menu page

--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,10 @@
 import jollof from './jollof-rice.jpg';
+import './menu.css';
 import pounded from './pounded-yam.jpg';
 import puff from './puff-puff.jpg';
 let createMenu = function () {
+	let main = document.createElement('div');
+	main.setAttribute('class', 'main');
 	let menuContainer = document.createElement('div');
 	menuContainer.setAttribute('id', 'menu-container');
 	for (let i = 0; i < 3; i++) {
@@ -11,28 +14,29 @@ let createMenu = function () {
 		let text = document.createElement('p');
 		if (i == 0) {
 			imageArea.src = `${jollof}`;
-			imageArea.style.width = '150px';
+			// imageArea.style.width = '150px';
 			text.textContent = 'Jollof Rice';
 			menuItem.appendChild(imageArea);
 			menuItem.appendChild(text);
 		}
 		if (i == 1) {
 			imageArea.src = `${pounded}`;
-			imageArea.style.width = '150px';
+			// imageArea.style.width = '150px';
 			text.textContent = 'Pounded Yam';
 			menuItem.appendChild(imageArea);
 			menuItem.appendChild(text);
 		}
 		if (i == 2) {
 			imageArea.src = `${puff}`;
-			imageArea.style.width = '150px';
+			// imageArea.style.width = '150px';
 			text.textContent = 'Puff Puff';
 			menuItem.appendChild(imageArea);
 			menuItem.appendChild(text);
 		}
 		menuContainer.appendChild(menuItem);
+		main.appendChild(menuContainer);
 	}
 
-	return menuContainer;
+	return main;
 };
 export default createMenu;
